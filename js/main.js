b1580e6b2d6ead15ef5a1fd591be8055768c70b3
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-04-25 15:50:30 +0200

    Add add, subtract, multiply and divide functions

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ let operation = '';
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
+
+function add(number) {
+    firstNumber += number;
+
+    return firstNumber;
+}
+
+function subtract(number) {
+    firstNumber -= number;
+
+    return firstNumber;
+}
+
+function multiply(number) {
+    firstNumber *= number;
+
+    return firstNumber;
+}
+
+function divide(number) {
+    if (number === 0) return 'To divide by zero is not defined';
+
+    firstNumber = Math.round(firstNumber / number * 1000) / 1000;
+
+    return firstNumber;
+}
