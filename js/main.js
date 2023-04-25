3ee323be28f99ae31ac568fa751ca2f9d7c43c48
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-04-25 15:27:39 +0200

    Add variables

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const currentOp = document.getElementById('current-op');
+const buttons = document.querySelectorAll('button');
+
+let operation = '';
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
